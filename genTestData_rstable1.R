11cc6828674f538607bcef3a37921b21cd09b8a0
Kiran Karra <kiran.karra@gmail.edu>
2016-03-21 23:02:05 -0400

    added testfiles to repo, added generator for rstable1 tests

---
+++ genTestData_rstable1.R
@@ -0,0 +1,38 @@
+#**************************************************************************
+#* 
+#* Copyright (C) 2016  Kiran Karra <kiran.karra@gmail.com>
+#*
+#* This program is free software: you can redistribute it and/or modify
+#* it under the terms of the GNU General Public License as published by
+#* the Free Software Foundation, either version 3 of the License, or
+#* (at your option) any later version.
+#*
+#* This program is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU General Public License for more details.
+#*
+#* You should have received a copy of the GNU General Public License
+#* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#**************************************************************************
+
+## A script which produces test data for rstable1, to compare w/ Matlab
+
+source("setupDir.R")
+
+n <- 1000
+alpha <- 0.8
+beta <- 1
+y <- rstable1(n, alpha, beta)
+
+write.table(y, file = "testfiles/rstable1_output1.csv", sep = ",", 
+            row.names = FALSE, col.names = FALSE)
+
+
+n <- 1000
+alpha <- 0.25
+beta <- 1
+y <- rstable1(n, alpha, beta)
+
+write.table(y, file = "testfiles/rstable1_output2.csv", sep = ",", 
+            row.names = FALSE, col.names = FALSE)
